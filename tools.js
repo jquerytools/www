1a3ec3ffdbe768fbf85a9fa014285b2aa1b661b8
Tero Piirainen <tipiirai@mitte.(none)>
2010-01-11 12:39:57 +0000

    new (shiny) home page and demo index

--- tools.js
+++ tools.js
@@ -8,7 +8,7 @@
 	key: 'jquery',
 	title: 'jQuery library',
 	description: 'All tools except flashembed are dependent on this library. Can alternatively be included from googleapis (see below)',
-	file: '/js/jquery',
+	file: 'jquery-1.3.2',
 	www: 'http://jquery.com'
 },
 
@@ -28,13 +28,12 @@
 },
 
 	{
-		key: 'slideshow',
+		key: 'tabs.slideshow',
 		title: 'Slideshow plugin',
 		plugin: true,
 		description: 'Transforms tabs into a working slideshow',
 		file: 'tabs/tabs.slideshow',
-		www: 'tabs/slideshow.html',
-		requires: 'Tabs'                      
+		www: 'tabs/slideshow.html'            
 	},
 
 
@@ -66,7 +65,6 @@
 	},
 
 
-//{{{ scrollable 
 {
 	key: 'scrollable',
 	title: 'Scrollable',
@@ -78,7 +76,7 @@
 
 	{
 		key: 'scrollable.circular',
-		title: 'Circular plugin',
+		title: 'Circular plugin (beta)',
 		plugin: true,
 		description: 'Makes an infinite loop from the scrollable items so that there is no beginning or end',
 		file: 'scrollable/scrollable.circular',
@@ -102,8 +100,6 @@
 		file: 'scrollable/scrollable.navigator',
 		www: 'scrollable/navigator.html'
 	},
-
-//}}}
 
 
 /* overlay */
@@ -136,7 +132,7 @@
 
 
 {
-	key: 'validator',
+	key: 'form.validator',
 	title: 'Validator',
 	description: '20 year wait is over. HTML5 is here',
 	file: 'form/form.validator',
@@ -144,28 +140,28 @@
 },
 
 {
-	key: 'styleable',
+	key: 'form.styleable',
 	title: 'Styleable',
-	description: 'Style your form widgets with CSS',
+	description: 'Form widgets + CSS made possible',
 	file: 'form/form.styleable',
 	www: 'form/styleable.html'
 },
 
 {
-	key: 'datepicker',
+	key: 'form.datepicker',
 	title: 'Datepicker',
-	description: 'Small, compact and styleable datepicker',
+	description: 'Small, compact, styleable HTML5 datepicker',
 	file: 'form/form.datepicker',
 	www: 'form/datepicker.html'
 },
 
 {
-	key: 'slider',
+	key: 'form.slider',
 	title: 'Slider',
 	description: 'HTML5 slider component',
 	file: 'form/form.slider',
 	www: 'form/slider.html',
-	requires: 'drag'
+	requires: 'form.drag'
 },
 //}}}
 
@@ -178,7 +174,7 @@
 },
 
 {
-	key: 'drag',
+	key: 'toolbox.drag',
 	title: 'Drag',
 	description: 'A generic drag support for jQuery',
 	file: 'toolbox/toolbox.drag',
@@ -186,7 +182,7 @@
 },
 
 {
-	key: 'flashembed',
+	key: 'toolbox.flashembed',
 	title: 'Flashembed',
 	description: 'The future of Flash embedding. Works as a standalone tool and jQuery is not required.',
 	file: 'toolbox/toolbox.flashembed',
@@ -194,7 +190,7 @@
 },
 
 {
-	key: 'history',
+	key: 'toolbox.history',
 	title: 'History',
 	description: 'Back button for AJAX apps',
 	file: 'toolbox/toolbox.history',
@@ -202,7 +198,7 @@
 },
 
 {
-	key: 'lazyload',
+	key: 'toolbox.lazyload',
 	title: 'Lazyload',
 	description: 'Load things only when needed.',
 	file: 'toolbox/toolbox.lazyload',
@@ -210,7 +206,7 @@
 },
 
 {
-	key: 'mask',
+	key: 'toolbox.mask',
 	title: 'Mask',
 	description: 'Makes your HTML stand out from its surroundings',
 	file: 'toolbox/toolbox.mask',
@@ -219,7 +215,7 @@
 
 
 {
-	key: 'mousewheel',
+	key: 'toolbox.mousewheel',
 	title: 'Mousewheel',
 	description: 'A generic mousewheel support for jQuery',
 	file: 'toolbox/toolbox.mousewheel',
