4653540ebe68489394890e58bd5398d0c65debf9
cdvrooman <enar@cheetah.webhostserver.biz>
2010-04-21 12:02:09 -0500

    Changed 'server side' to 'server-side'

--- demos/demos.js
+++ demos/demos.js
@@ -1,222 +1,222 @@
 {
-	title: 'ui',	
-	
-//{{{ UI 
+	title: 'ui',
 
-	
+//{{{ UI
+
+
 //{{{ Tabs
-		
+
 	'1. Tabs': {
-	
+
 		description: 'The most important UI widget for the web',
 		keywords: 'javascript tabs, jquery tabs, javascript tabbed navigation, ajax tabs, tab history, jquery accordion',
-		
+
 		demos: [
-			{		
+			{
 				path: 'tabs/index.html',
 				title: 'Minimal setup for Tabs'
-			}, 
-			{		
+			},
+			{
 				path: 'tabs/anchors.html#second',
 				title: 'Naming the tabs'
-			}, 			
-			{		
+			},
+			{
 				path: 'tabs/skins.html',
 				title: '4 different skins with CSS'
-			},			
-			{		
+			},
+			{
 				path: 'tabs/mouseover.html',
 				title: 'Using mouseover to switch tabs'
-			},		 
-			{		
+			},
+			{
 				path: 'tabs/wizard.html',
 				title: 'Making Wizards with the Tabs'
-			},				
-			{		
+			},
+			{
 				path: 'tabs/accordion.html',
 				title: 'Making Accordions with the Tabs',
 				keywords: 'javascript accordion, jquery accordion plugin, accordion history'
-			},				
-			{		
+			},
+			{
 				path: 'tabs/accordion-custom.html',
 				title: 'Customizing the Accordion effect',
 				keywords: 'javascript accordion, jquery accordion plugin, accordion effects'
-			},				
-			{		
+			},
+			{
 				path: 'tabs/accordion-horizontal.html',
 				title: 'Horizontal Accordion using the Tabs',
 				keywords: 'horizontal accordion, jquery horizontal accordion, horizontal accordion plugin'
-			},		
-			{		
+			},
+			{
 				path: 'tabs/multiple.html',
 				title: 'Multiple Tabs and Accordion instances',
 				keywords: 'nested tabs, multiple accordions, nested accordions'
-			},			
-			{		
+			},
+			{
 				path: 'tabs/history.html',
 				title: 'Handling browsers back button'
-			}, 
-			{		
+			},
+			{
 				path: 'tabs/ajax.html',
 				title: 'Loading tab contents with AJAX'
-			}, 
-			{		
+			},
+			{
 				path: 'tabs/ajax-history.html',
 				title: 'AJAX:ed tabs with History support'
-			}, 			
-			{		
+			},
+			{
 				path: 'tabs/slideshow.html',
 				title: 'Slideshow plugin for the Tabs',
 				keywords: 'jquery slideshow, javascript slideshow, javascript crossfade, slideshow plugin'
-			} 			
-		] 
+			}
+		]
 	},
 //}}}
 
 
-//{{{ Tooltip 
-		
+//{{{ Tooltip
+
 	'2. Tooltips': {
-	
+
 		description: 'The second most important UI widget',
 		keywords: 'tooltips, javascript tooltip, jquery tooltip, jquery tooltip plugin, javascript tooltips, jquery tooltips',
-		
-		demos: [	
-			{		
+
+		demos: [
+			{
 				path: 'tooltip/index.html',
 				title: 'Basics of using the tooltip'
-			},		
-			{		
+			},
+			{
 				path: 'tooltip/any-html.html',
 				title: 'Using any HTML inside the tooltip'
-			},		
-			{		
+			},
+			{
 				path: 'tooltip/imitate.html',
 				title: 'Imitating browsers default tooltip'
-			},			
-			{		
+			},
+			{
 				path: 'tooltip/form.html',
 				title: 'Using tooltips in form fields'
-			},				
-			{		
+			},
+			{
 				path: 'tooltip/table.html',
 				title: 'Using tooltips in tables or lists'
-			},		
-			{		
+			},
+			{
 				path: 'tooltip/custom-effect.html',
 				title: 'Custom tooltip effect'
-			},		
-			{		
+			},
+			{
 				path: 'tooltip/dynamic.html',
 				title: 'Dynamic positioning of the tooltip'
-			}		
-		] 
-	},		
+			}
+		]
+	},
 //}}}
 
 
 //{{{ Overlay
 
 	'3. Overlay': {
-	
+
 		description: 'Overlay your HTML with eye candy',
 		keywords: 'javascript overlay,jquery lightbox,jquery applebox,jquery overlay,jquery overlayed content,applebox,apple box',
-		
-		demos: [	
-			{		
+
+		demos: [
+			{
 				path: 'overlay/index.html',
 				title: 'Minimal setup for overlay'
-			},				
-			{		
+			},
+			{
 				path: 'overlay/apple.html',
 				title: 'The Apple effect for overlay'
 			},
-			{		
+			{
 				path: 'overlay/modal-dialog.html',
 				title: 'Creating modal dialogs with overlay'
-			},				
-			{		
+			},
+			{
 				path: 'overlay/trigger.html',
 				title: 'Opening overlays programmatically'
-			},							
-			{		
+			},
+			{
 				path: 'overlay/styling.html',
 				title: 'Overlays with different styles'
-			},	
-			{		
+			},
+			{
 				path: 'overlay/external.html',
 				title: 'Loading external pages into overlay'
-			},				
-			{		
+			},
+			{
 				path: 'overlay/multiple.html',
 				title: 'Multiple overlays on a same page'
-			},		
-			{		
+			},
+			{
 				path: 'overlay/custom-effect.html',
 				title: 'Creating a customized Overlay effect'
-			}		
-		] 
+			}
+		]
 	},
-	
+
 //}}}
 
 
-//{{{ Scrollable 
+//{{{ Scrollable
 
 	'4. Scrollable': {
-	
+
 		description: 'Scroll your HTML with eye candy',
 		keywords: 'jquery scrolling,jquery scrollable,javascript scrolling,javascript scrolling plugin,jquery scrolling plugin,jquery scrollable plugin,scrolling html elements',
-		
-		demos: [	
-			{		
+
+		demos: [
+			{
 				path: 'scrollable/index.html',
 				title: 'Minimal setup for scrollable'
-			},			
-			{		
+			},
+			{
 				path: 'scrollable/vertical.html',
 				title: 'A vertical scrollable'
-			},			
-			{		
+			},
+			{
 				path: 'scrollable/gallery.html',
 				title: 'A simple scrollable image gallery'
-			},		
-			{		
+			},
+			{
 				path: 'scrollable/multiple-scrollables.html',
 				title: 'Another gallery with many scrollables'
-			},								
-			{		
+			},
+			{
 				path: 'scrollable/wizard.html',
 				title: 'A scrollable registration wizard'
-			},					
-			{		
+			},
+			{
 				path: 'scrollable/plugins/index.html',
 				title: 'Scrollable plugins in action',
 				keywords: 'scrollable plugins, mousewheel, circular, autoscroll, navigator'
-			},			
-			{		
+			},
+			{
 				path: 'scrollable/one-sized.html',
 				title: 'Navigation with browser\'s back button',
 				keywords: 'scrollable history, scrollable back button, scroll back button'
 			},
-			{		
+			{
 				path: 'scrollable/home.html',
-				title: 'jQuery Tools home page setup'				
+				title: 'jQuery Tools home page setup'
 			},
-			{		
+			{
 				path: 'scrollable/site-navigation.html',
 				title: 'A complete scrollable navigational system',
-				keywords: 'scrollable site navigation, scrollable navigation, keyboard scrollable, arrow scroll'  
-			},						
-			{		
+				keywords: 'scrollable site navigation, scrollable navigation, keyboard scrollable, arrow scroll'
+			},
+			{
 				path: 'scrollable/edit.html',
 				title: 'Add and remove items from scrollable'
-			},						
-			{		
+			},
+			{
 				path: 'scrollable/easing.html',
 				title: 'Customizing the scrolling animation'
-			}		
-		] 
+			}
+		]
 	},
 
 //}}}
@@ -227,157 +227,157 @@
 },
 
 {
-	title: 'form',	
+	title: 'form',
 
-//{{{ FORM 
+//{{{ FORM
 
 	'1. Validator': {
-	
+
 		description: 'HTML5 form validation made easy',
 		keywords: 'jquery validator, jquery form validation, jquery validation, form validation, html5 form validation',
-    
+
 		demos: [
-			{		
+			{
 				path: 'validator/index.html',
 				title: 'Minimal setup for Validator'
 			},
-			{		
+			{
 				path: 'validator/custom-validators.html',
 				title: 'Custom input types and attributes'
 			},
-			{		
+			{
 				path: 'validator/server-side.html',
-				title: 'Combined server- and client side validation'
-			},			
-			{		
-				path: 'validator/custom-effect.html',
-				title: 'Displaying all errors inside single DIV'
+				title: 'Combined server and client-side validation'
 			},
-			{		
+			{
+				path: 'validator/custom-effect.html',
+				title: 'Displaying all errors inside a single DIV'
+			},
+			{
 				path: 'validator/events.html',
 				title: 'Validator events in action'
-			},			
-			{		
+			},
+			{
 				path: 'validator/localize.html',
 				title: 'Localizing the Validator (finnish)'
-			}			
+			}
 		]
 	},
-	
-/* 
+
+/*
 	'2. Styleable': {
-	
+
 		description: 'Form widgets + CSS made possible',
 		keywords: 'form css, styleable forms, customized form look, styleable forms, form skinning',
-    
+
 		demos: [
-			{		
+			{
 				path: 'styleable/index.html',
 				title: 'Minimal setup for Styleable'
 			},
-			{		
+			{
 				path: 'styleable/scripting.html',
 				title: 'Scripting styleable Forms'
 			},
-			{		
+			{
 				path: 'styleable/skin.html',
 				title: 'Alternate form skin'
 			}
 		]
-	}, 
+	},
 */
 
 
 	'2. Rangeinput': {
-	
+
 		description: 'HTML5 range input for humans',
 		keywords: 'HTML5 range input, jquery range, jquery rangeinput, javascript range input, skinnable rangeinput',
-		
+
 		demos: [
-			{		
+			{
 				path: 'rangeinput/index.html',
 				title: 'Minimal setup for rangeinput'
 			},
-			{		
+			{
 				path: 'rangeinput/vertical.html',
 				title: 'Couple of vertical ranges'
 			},
-			{		
+			{
 				path: 'rangeinput/multiple.html',
 				title: 'Many, many small ranges'
 			},
-			{		
+			{
 				path: 'rangeinput/scrollbar.html',
 				title: 'A custom scrollbar for a DIV'
 			}
 		]
 	},
-	
+
 	'3. Dateinput': {
-	
+
 		description: 'HTML5 date input for humans',
 		keywords: 'jquery dateinput, jquery dateinput, HTML5 date input, javascript date input, skinnable dateinput',
-		
+
 		demos: [
-			{		
+			{
 				path: 'dateinput/index.html',
 				title: 'Minimal setup for Dateinput'
 			},
-			{		
+			{
 				path: 'dateinput/large-skin.html',
 				title: 'A large skin for Dateinput'
-			},			
-			{		
+			},
+			{
 				path: 'dateinput/customize.html',
 				title: 'Customizing Dateinput behaviour'
 			},
-			{		
+			{
 				path: 'dateinput/flight.html',
 				title: 'Prompting for start and end dates'
-			},				
-			{		
+			},
+			{
 				path: 'dateinput/localize.html',
 				title: 'Localizing the Dateinput (french)'
-			}			
+			}
 		]
-	},	 
+	},
 
-	
+
 //}}}
 
 },
 
 {
-	title: 'toolbox',	
-	
+	title: 'toolbox',
+
 //{{{ TOOLBOX
 
 
 //{{{ Expose
 
 	'1. Expose': {
-	
+
 		description: 'Make your HTML elements stand out',
 		keywords: 'mask html,expose html jquery,javascript mask,stand out html javascript,javascript mask plugin',
-		
-		demos: [	
-			{		
+
+		demos: [
+			{
 				path: 'toolbox/expose/index.html',
 				title: 'Minimal setup for expose'
-			},	
-			{		
+			},
+			{
 				path: 'toolbox/expose/styling.html',
 				title: 'Styling the mask with a background image'
-			},			
-			{		
+			},
+			{
 				path: 'toolbox/expose/form.html',
 				title: 'Exposing a form'
-			},			
-			{		
+			},
+			{
 				path: 'toolbox/expose/video.html',
 				title: 'Exposing videos with a custom mask'
 			}
-		] 
+		]
 	},
 //}}}
 
@@ -385,74 +385,74 @@
 //{{{ Flashembed
 
 	'2. Flashembed': {
-	
+
 		description: 'The Future of Flash embedding',
 		keywords: 'javascript tabs, jquery tabs, javascript tabbed navigation, ajax tabs, tab history',
-		
-		demos: [	
-			{		
+
+		demos: [
+			{
 				path: 'toolbox/flashembed/index.html',
 				title: 'Basics of Flash embedding'
-			},		
-			
-			{		
+			},
+
+			{
 				path: 'toolbox/flashembed/jquery.html',
 				title: 'Flashembed and jQuery'
 			},
-			
-			{		
+
+			{
 				path: 'toolbox/flashembed/onclick.html',
 				title: 'Loading Flash on mouse click'
-			}, 
-			
-			{		
+			},
+
+			{
 				path: 'toolbox/flashembed/wmode.html',
 				title: 'Placing HTML on top of a Flash object'
-			},	 
-			
-			{		
+			},
+
+			{
 				path: 'toolbox/flashembed/fail.html',
 				title: 'Handling old Flash versions'
-			},		
-	
-			{		
+			},
+
+			{
 				path: 'toolbox/flashembed/flowplayer.html',
 				title: 'Flashembed and Flowplayer'
-			}		
-		] 
+			}
+		]
 	},
-	
-//}}}
-
 
 //}}}
 
 
-//{{{ Combinations 
+//}}}
+
+
+//{{{ Combinations
 
 	'Combinations': {
-	
+
 		description: 'Possibilities are endless. Get creative!',
 		keywords: 'javascript image gallery, javascript portfolio, image gallery, jquery image gallery',
-		
-		demos: [			
-			{		
+
+		demos: [
+			{
 				path: 'combine/html5-form/index.html',
-				title: 'HTML5 form inside an overlay'				
-			},		
-			
-			{		
+				title: 'HTML5 form inside an overlay'
+			},
+
+			{
 				path: 'combine/portfolio/index.html',
-				title: 'An artist\'s portfolio'				
-			},		
-			
-			{		
+				title: 'An artist\'s portfolio'
+			},
+
+			{
 				path: 'combine/portfolio/preload.html',
 				title: 'Speeding up the portfolio'
-			}		
-		] 
+			}
+		]
 	}
-	
+
 //}}}
 
 
